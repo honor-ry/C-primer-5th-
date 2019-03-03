ddbc6c87f38f1ad7ffd81dfd5cee594e18a7dfec
honor-ry <995688198@qq.com>
2019-03-03 09:37:58 +0800

    第二次修改

--- exam.cpp
+++ exam.cpp
@@ -22,46 +22,6 @@ vector<string> vec;//声明一个vector
 ifstream in(infile);//ifstream定义了一个输入流in(文件流)，它被初始化从文件中读取数据
 istringstream iss("hello");
 func(iss);
-if (in)//检查文件的读取是否成功,养成良好的习惯！
-{
-    string buf;
-    while (in >> buf)//每个单词的独立输入，自动以空格为间隔
-    {
-        vec.push_back(buf);
-    }
-}
-else
-{
-    cerr<<"cannot open this file: "<<infile<<endl;
-}
-for (int i = 0;i < vec.size();++i)
-{
-    cout<<vec[i]<<endl;
-}
-for (int i = 0;i < vec.size();++i)
-{
-    cout<<vec[i]<<endl;
-}
-for (int i = 0;i < vec.size();++i)
-{
-    cout<<vec[i]<<endl;
-}
-for (int i = 0;i < vec.size();++i)
-{
-    cout<<vec[i]<<endl;
-}
-for (int i = 0;i < vec.size();++i)
-{
-    cout<<vec[i]<<endl;
-}
-for (int i = 0;i < vec.size();++i)
-{
-    cout<<vec[i]<<endl;
-}
-for (int i = 0;i < vec.size();++i)
-{
-    cout<<vec[i]<<endl;
-}
 
 
 return 0;
